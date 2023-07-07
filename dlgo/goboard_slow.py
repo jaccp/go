6c9fc924c5d0d686f04819a49c06dddc07b6b214
Jack <jackpalmer96@gmail.com>
2023-07-08 00:28:20 +0100

    "added play, pass_turn and resign to Move()"

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -7,7 +7,21 @@ class Move():
         # assert must return True for the code to continue. 
         # ^ is a bitwise exclusive OR operator, XOR, which deals only in 1s / 0s - also read as True / False. 
         # example: a ^ b means that a or b can be True to return a logical True, but they cant both be True. 
+        # our assertion means that ONLY one of these can be True
         self.point = point
         self.is_play = (self.point is not None)
         self.is_pass = is_pass
         self.is_resign = is_resign
+
+    @classmethod # we use the classmethod decorator to define a class method, which means we as defining at class level and not instance level
+    def play(cls, point):
+        return Move(point=point)
+    
+    @classmethod
+    def pass_turn(cls):
+        return Move(is_pass=True)
+    
+    @classmethod
+    def resign(cls):
+        return Move(is_resign=True)
+    
--- dlgo/gotypes.py
+++ dlgo/gotypes.py
@@ -3,7 +3,7 @@ import enum
 from collections import namedtuple
 
 # define our player class
-class player(enum.Enum):
+class Player(enum.Enum):
     # we are using an enumaration class for both of our players
     # this means we have two playeres, 1 and 2, corresponding to black and white, on the same class object
     black = 1
@@ -11,19 +11,19 @@ class player(enum.Enum):
 
     @property # we use the property decorator to define a method that can be called as an attribute
     def other(self):
-        return player.black if self == player.white else player.white
+        return Player.black if self == Player.white else Player.white
         # this method allows us to see which player is our opponent 
 
-class point(namedtuple('point', ['row', 'col'])):
+class Point(namedtuple('point', ['row', 'col'])):
     # namedtuple allows us to access the coordinates of our point
     # using point.row and point.col instead of point[0] and point[1]
     def neighbors(self):
         # define the neighbord of the point as those that are directly above,
         # below, left and right of the point
         return [
-            point(self.row - 1, self.col),
-            point(self.row + 1, self.col),
-            point(self.row, self.col - 1),
-            point(self.row, self.col + 1)
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1)
         ]
 

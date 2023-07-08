3bd64a6d176554d173697939c84335e3fa8ed01f
Jack <jackpalmer96@gmail.com>
2023-07-08 12:54:41 +0100

    "added Board class"

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -60,3 +60,10 @@ class GoString():
             self.color == other.color and \
             self.stones == other.stones and \
             self.liberties == other.liberties
+    
+class Board():
+    # our board is initialised as an empty grid with the specified number of rows and columns
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {}
